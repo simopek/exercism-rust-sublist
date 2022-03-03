28d480e57a8a348fa3cca7f46674c91289edb205
simopek <>
2022-03-03 23:04:20 +0100

    Add improved solution

--- src/lib.rs
+++ src/lib.rs
@@ -9,6 +9,20 @@ pub enum Comparison {
 }
 
 pub fn sublist<T: PartialEq>(_first_list: &[T], _second_list: &[T]) -> Comparison {
+    let superlist = _second_list.is_empty() ||
+        _first_list.windows(_second_list.len()).any(|x| x == _second_list);
+    let sublist = _first_list.is_empty() ||
+        _second_list.windows(_first_list.len()).any(|x| x == _first_list);
+
+    match (sublist, superlist) {
+        (true, true) => Comparison::Equal,
+        (true, false) => Comparison::Sublist,
+        (false, true) => Comparison::Superlist,
+        _ => Comparison::Unequal
+    }
+}
+
+pub fn sublist_old<T: PartialEq>(_first_list: &[T], _second_list: &[T]) -> Comparison {
     if list_a_is_sublist_of_list_b(_first_list, _second_list) {
         if _first_list.len() == _second_list.len() {
             Equal
